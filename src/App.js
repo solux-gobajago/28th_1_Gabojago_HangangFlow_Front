79f3a0520d8d5f3af58172874b87ccb07b78f9d0
yoonseo <decelev1211@gmail.com>
2023-07-21 18:21:25 +0900

    네이버 api에서 데이터 받아오기 성공(기본 검색어 여의도 한강공원)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    // Function to fetch data from the API using a CORS proxy
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/v1/search/local.json", {
+          params: {
+            query: "여의도한강공원",
+            display:1
+          },
+          headers: {
+            'X-Naver-Client-Id': '6U2rWS_H78sSzQhpQHpT',
+            'X-Naver-Client-Secret': 'V6_0mvK1kF'
+          }
+        });
+        console.log(response.data); // Process the response data
+      }catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    // Call the fetchData function when the component mounts
+    fetchData();
+  }, []); // Empty dependency array to run the effect only once when the component mounts
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Test
-        </a>
-      </header>
+      babo
+      wa~~~~~~~~~~~~~
     </div>
   );
 }

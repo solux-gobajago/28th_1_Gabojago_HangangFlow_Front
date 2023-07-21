2375a8677d0f90662ff58b183982464c1293eeb3
yoonseo <decelev1211@gmail.com>
2023-07-21 19:23:51 +0900

    화면에 공원 정보 표시

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
-import { useEffect } from 'react';
 
 function App() {
+  const [responseData, setResponseData] = useState(null);
+
   useEffect(() => {
     // Function to fetch data from the API using a CORS proxy
     const fetchData = async () => {
       try {
         const response = await axios.get("/v1/search/local.json", {
           params: {
-            query: "여의도한강공원",
-            display:1
+            query: "망원한강공원",
+            display: 1
           },
           headers: {
             'X-Naver-Client-Id': '6U2rWS_H78sSzQhpQHpT',
@@ -18,19 +20,36 @@ function App() {
           }
         });
         console.log(response.data); // Process the response data
-      }catch (error) {
+        setResponseData(response.data); // Save the response data to the state
+
+        
+      } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
-
+  
     // Call the fetchData function when the component mounts
     fetchData();
   }, []); // Empty dependency array to run the effect only once when the component mounts
 
   return (
     <div className="App">
-      babo
-      wa~~~~~~~~~~~~~
+      {responseData ? (
+      <div>
+        
+        {responseData.items.map((item, index) => (
+          <h1 key={index}>{item.title.replace("<b>","").replace("</b>","")}</h1>
+        ))}
+        {responseData.items.map((item, index) => (
+          <h1 key={index}>{item.address}</h1>
+        ))}
+        {responseData.items.map((item, index) => (
+          <h1 key={index}>{item.link}</h1>
+        ))}
+      </div>
+    ) : (
+      <p>Loading...</p>
+    )}
     </div>
   );
 }
